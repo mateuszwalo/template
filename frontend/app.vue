b88f6565490466ac807fecfc5a54b3694aa10c79
git-maks <maaks.koziel@gmail.com>
2025-04-23 16:59:13 +0000

    test comit to main

---
+++ frontend/app.vue
@@ -0,0 +1,9 @@
+# test frontend
+
+<template>
+  <div id="app">
+    <h1>Welcome to Your Vue.js App</h1>
+    <p>Current time: {{ currentTime }}</p>
+    <p>API Response: {{ apiResponse }}</p>
+  </div>
+</template>
